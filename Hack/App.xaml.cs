f8e653fd7ae2264e5ddc34656bf2ae32806e61c2
VasyGood <balasnikovv3@gmail.com>
2024-11-17 18:58:00 +0300

    Добавьте файлы проекта.

---
+++ Hack/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Hack
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Hack" };
+        }
+    }
+}
